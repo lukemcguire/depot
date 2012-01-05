0623a952c320dbaa8ec27fcfdb55dac91e7ea554
Luke McGuire <luke.mcguire@gmail.com>
2012-01-05 00:25:08 -0800

    Task H

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,18 @@ Depot::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-end
+  # SMTP configuration for gmail
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:        "smtp.gmail.com" ,
+    port:           "587",
+    domain:         "gmail.com",
+    authentication: "plain",
+    user_name:      "luke.mcguire@gmail.com",
+    password:       "gnofrsslcauzsbpw",
+    enable_starttls_auto: true
+  }
+end  
+
